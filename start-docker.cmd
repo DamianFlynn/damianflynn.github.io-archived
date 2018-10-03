9d738c3b17851433a8dba4ebbaadf50bc9d02bc5
Damian Flynn <technology@damianflynn.com>
2018-10-03 22:36:20 +0100

    Line Ending changes

--- start-docker.cmd
+++ start-docker.cmd
@@ -1,2 +1,2 @@
-docker run --rm -it -p 4000:4000 -v ${PWD}:/srv/jekyll -e "JEKYLL_ENV=production" jekyll/jekyll bash -c "bundle install && jekyll serve --incremental --watch --force_polling"
+docker run --rm -it -p 4000:4000 -v $PWD:/srv/jekyll -e "JEKYLL_ENV=production" jekyll/jekyll bash -c "bundle install && jekyll serve --incremental --watch --force_polling"
 
